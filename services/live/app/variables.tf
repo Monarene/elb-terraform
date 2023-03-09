86061d6b92fdef17f74f70b01000346bc20c1220
Michael Mekuleyi <mekuleyimichael@gmail.com>
2023-03-09 15:44:34 +0100

    removed larged files

---
+++ services/live/app/variables.tf
@@ -0,0 +1,84 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "server_text" {
+  description = "The text the web server should return"
+  default     = "Hello, World!"
+  type        = string
+}
+
+variable "environment" {
+  description = "The name of the environment we're deploying to"
+  type        = string
+  default     = "prod"
+}
+
+variable "ami" {
+  description = "The AMI to run in the cluster"
+  type        = string
+  default = "ami-0fb653ca2d3203ac1"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+  default = "t2.micro"
+
+  validation {
+    condition     = contains(["t2.micro", "t3.micro"], var.instance_type)
+    error_message = "Only free tier is allowed: t2.micro | t3.micro."
+  }
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+  default = 1
+
+  validation {
+    condition     = var.min_size > 0
+    error_message = "ASGs can't be empty or we'll have an outage!"
+  }
+
+  validation {
+    condition     = var.min_size <= 10
+    error_message = "ASGs must have 10 or fewer instances to keep costs down."
+  }
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+  default = 10
+
+  validation {
+    condition     = var.max_size > 0
+    error_message = "ASGs can't be empty or we'll have an outage!"
+  }
+
+  validation {
+    condition     = var.max_size <= 10
+    error_message = "ASGs must have 10 or fewer instances to keep costs down."
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "enable_autoscaling" {
+  description = "If set to true, enable auto scaling"
+  type        = bool
+  default = true
+}
+
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
+
